b700adfe5a5b17dda3d40bb9677494e35b14f5f0
evanm510 <evanm510@gmail.com>
2025-05-27 15:04:42 -0700

    fix motion box error

--- components/motion/box.tsx
+++ components/motion/box.tsx
@@ -1,10 +1,22 @@
-import { ChakraProps, chakra } from '@chakra-ui/react'
-import { HTMLMotionProps, motion } from 'framer-motion'
+import { ChakraProps, HTMLChakraProps, chakra } from '@chakra-ui/react'
+import { MotionProps, motion } from 'framer-motion'
 
-export interface MotionBoxProps
-  extends Omit<HTMLMotionProps<'div'>, 'children' | 'style'>,
-    Omit<ChakraProps, 'transition' | 'color'> {
-  children?: React.ReactNode
-}
+import React from 'react'
 
-export const MotionBox = motion.create(chakra.div)
+// Combine Chakra and Framer Motion props for a div element
+export type MotionBoxProps = HTMLChakraProps<'div'> & MotionProps
+
+// Create Chakra styled div
+const ChakraDiv = chakra('div')
+
+// Name the inner function to satisfy ESLint
+const MotionBoxComponent = React.forwardRef<HTMLDivElement, MotionBoxProps>(
+  function MotionBoxComponent(props, ref) {
+    return <ChakraDiv ref={ref} {...props} />
+  },
+)
+
+// Wrap with Framer Motion
+export const MotionBox = motion(MotionBoxComponent)
+
+MotionBox.displayName = 'MotionBox'
