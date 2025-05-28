8fcbe60cdfdc109227e60004cde43c4048f5d5fc
evanm510 <evanm510@gmail.com>
2025-05-28 11:26:07 -0700

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -16,31 +16,53 @@ export interface FooterProps extends BoxProps {
   columns?: number
 }
 
+// export const Footer: React.FC<FooterProps> = (props) => {
+//   const { columns = 2, ...rest } = props
+//   return (
+//     <Box bg="white" _dark={{ bg: 'gray.900' }} {...rest}>
+//       <Container maxW="container.2xl" px="8" py="8">
+//         <SimpleGrid columns={columns}>
+//           <Stack spacing="8">
+//             <Stack alignItems="flex-start">
+//               <Flex>
+//                 <Box as={siteConfig.logo} flex="1" height="32px" />
+//               </Flex>
+//               <Text fontSize="md" color="muted">
+//                 {siteConfig.seo.description}
+//               </Text>
+//             </Stack>
+//             <Copyright>{siteConfig.footer.copyright}</Copyright>
+//           </Stack>
+//           <HStack justify="flex-end" spacing="4" alignSelf="flex-end">
+//             {siteConfig.footer?.links?.map(({ href, label }) => (
+//               <FooterLink key={href} href={href}>
+//                 {label}
+//               </FooterLink>
+//             ))}
+//           </HStack>
+//         </SimpleGrid>
+//       </Container>
+//     </Box>
+//   )
+// }
+
 export const Footer: React.FC<FooterProps> = (props) => {
-  const { columns = 2, ...rest } = props
+  const { ...rest } = props
+
   return (
-    <Box bg="white" _dark={{ bg: 'gray.900' }} {...rest}>
-      <Container maxW="container.2xl" px="8" py="8">
-        <SimpleGrid columns={columns}>
-          <Stack spacing="8">
-            <Stack alignItems="flex-start">
-              <Flex>
-                <Box as={siteConfig.logo} flex="1" height="32px" />
-              </Flex>
-              <Text fontSize="md" color="muted">
-                {siteConfig.seo.description}
-              </Text>
-            </Stack>
-            <Copyright>{siteConfig.footer.copyright}</Copyright>
-          </Stack>
-          <HStack justify="flex-end" spacing="4" alignSelf="flex-end">
-            {siteConfig.footer?.links?.map(({ href, label }) => (
-              <FooterLink key={href} href={href}>
-                {label}
-              </FooterLink>
-            ))}
-          </HStack>
-        </SimpleGrid>
+    <Box bg="gray.50" _dark={{ bg: 'gray.900' }} {...rest}>
+      <Container
+        maxW="container.2xl"
+        px={{ base: 4, md: 8 }}
+        py={{ base: 10, md: 12 }}
+      >
+        <Stack spacing={4} align="center" textAlign="center">
+          <Box as={siteConfig.logo} height="32px" />
+          <Text fontSize="xs" color="gray.500" _dark={{ color: 'gray.600' }}>
+            &copy; {new Date().getFullYear()} Gans-Monroe Consulting. All rights
+            reserved.
+          </Text>
+        </Stack>
       </Container>
     </Box>
   )
